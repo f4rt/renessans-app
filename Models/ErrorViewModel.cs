f3b5dee0412a00190976fb0487354ec269dfad31
Sergey Klenin <klenin@ims58.ru>
2019-10-17 11:40:14 +0300

    Init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RenessansApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
